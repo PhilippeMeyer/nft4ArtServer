a8f414cb85657e9f7f65674bd196dd55846247e4
GPPM03 <philippe.meyer@gazprombank.ch>
2022-08-29 17:06:22 +0200

    Adding some tests cases on the price updates

--- tests/priceUpdate.ts
+++ tests/priceUpdate.ts
@@ -57,6 +57,78 @@ describe("Setting a token's price", function() {
         expect(ret4[0].price).to.be.equal(price);
     });
 
+    it('Logging in as a manager with the correct password and setting a price on a non existing token ', async function () {
+
+        const res = await fetch(urlLogin, {
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify(resp)
+        });
+
+        expect(res.status).to.equal(200);
+        const ret = await res.json();
+        expect (ret.accessToken).not.to.be.undefined;
+
+        if(res.status == 200) jwt = ret.accessToken;
+        const jwtHeader = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'authorization': 'Bearer ' + jwt };
+
+        urlSetPrice += '?price=' + price.toString() + '&tokenId=123';
+        const res2 = await fetch(urlSetPrice, { method: 'PUT', headers: jwtHeader } );
+        expect(res2.status).to.equal(404);
+    });
+
+    it('Logging in as a manager with the correct password and not sending the price in the request', async function () {
+
+        const res = await fetch(urlLogin, {
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify(resp)
+        });
+
+        expect(res.status).to.equal(200);
+        const ret = await res.json();
+        expect (ret.accessToken).not.to.be.undefined;
+
+        if(res.status == 200) jwt = ret.accessToken;
+        const jwtHeader = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'authorization': 'Bearer ' + jwt };
+
+        const res2 = await fetch(urlTokens, { method: 'GET', headers: jwtHeader } );
+        const ret2 = await res2.json();
+        expect(res2.status).to.equal(200);
+        expect(ret2).to.have.lengthOf.above(0);
+        if (ret2.length == 0) return;
+
+        urlSetPrice += '?tokenId=' + ret2[0].id;
+        const res3 = await fetch(urlSetPrice, { method: 'PUT', headers: jwtHeader } );
+        expect(res3.status).to.equal(400);
+    });
+
+    it('Logging in as a manager with the correct password and not sending the token in the request', async function () {
+
+        const res = await fetch(urlLogin, {
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify(resp)
+        });
+
+        expect(res.status).to.equal(200);
+        const ret = await res.json();
+        expect (ret.accessToken).not.to.be.undefined;
+
+        if(res.status == 200) jwt = ret.accessToken;
+        const jwtHeader = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'authorization': 'Bearer ' + jwt };
+
+        const res2 = await fetch(urlTokens, { method: 'GET', headers: jwtHeader } );
+        const ret2 = await res2.json();
+        expect(res2.status).to.equal(200);
+        expect(ret2).to.have.lengthOf.above(0);
+        if (ret2.length == 0) return;
+
+        urlSetPrice += '?price=100';
+        const res3 = await fetch(urlSetPrice, { method: 'PUT', headers: jwtHeader } );
+        expect(res3.status).to.equal(400);
+    });
+
     //TODO Test if the message has been received
 });
 
