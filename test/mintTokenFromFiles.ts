ac9b42bbf753a669e5fd3c4c109e095588e867a3
PhilMeyer <philippe@famille-meyer.net>
2022-08-31 00:31:49 +0200

    Use chai request instead of fetch

--- test/mintTokenFromFiles.ts
+++ test/mintTokenFromFiles.ts
@@ -1,16 +1,39 @@
-import fetch from 'node-fetch';
 import fs from 'fs';
 import FormData from 'form-data';
-
+import { expect } from "chai";
+import chai from "chai";
+import chaiHttp from 'chai-http';
 
 const server = 'http://localhost:8999';
-const urlMint = server + '/apiV1/token/mintTokenFromFiles';
+const image = './test/testImage.jpg'
+const model = './test/appLogin.js'
+const urlMint = '/apiV1/token/mintTokenFromFiles';
 
-var formData = new FormData();
-let readStream = fs.createReadStream('appLogin.ts');
-formData.append('files', readStream);
-formData.append('files', readStream);
-formData.append('files', readStream);
-formData.append('property', 'model1');
-formData.append('property', 'model2');
-const res = await fetch(urlMint, { method: 'POST', body: formData});
+chai.use(chaiHttp);
+
+describe('Minting tokens', function() {
+    it('Sending a picture and a model to be minted', function (done) {
+
+/*
+        let imgStream = fs.createReadStream(image);
+        let mdlStream = fs.createReadStream('appLogin.js');
+        var formData = new FormData();
+        formData.append('image_raw', imgStream);
+        formData.append('files', mdlStream);
+        formData.append('description', 'Sweet spot');
+        formData.append('author', 'Creative Commons');
+        formData.append('name', 'Paradise');
+*/
+        chai.request(server)
+        .post(urlMint)
+        .type('form')
+        .attach('image_raw', image, image)
+        .attach('files', model, model)
+        .field('description', 'Sweet spot')
+        .field('author', 'Creative Commons')
+        .then((res) => {
+            expect(res).to.have.status(200);
+            done();
+        });
+    });
+})
