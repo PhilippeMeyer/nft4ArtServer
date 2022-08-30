8b4f96b025ab6bdab03fc8d430f199f694102574
PhilMeyer <philippe@famille-meyer.net>
2022-08-30 21:46:57 +0200

    moving tests into test folder

--- endPoints/token/mintTokenFromFiles.ts
+++ endPoints/token/mintTokenFromFiles.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import sharp from 'sharp';
 
 
 async function mintTokenFromFiles(req: Request, res: Response){
     console.log(req.files);
     console.log(req.body);
+    //const fileInput: object[] = req.files as object[];
+    //fileInput.forEach((file: sharp.SharpOptions | undefined) => {sharp(file).resize({width:100}).toFile('test')});
     res.sendStatus(200);
 }
 
